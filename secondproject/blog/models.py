bbd6fb69872dc396c0fe3db4693c1d03d7ba5216
caesiumy <mn0658@naver.com>
2019-01-22 22:00:53 +0900

    title str modified

--- secondproject/blog/models.py
+++ secondproject/blog/models.py
@@ -5,3 +5,6 @@ class blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
