82fee6714163151d5fc67fc9a2d9ac77a96f365f
caesiumy <mn0658@naver.com>
2019-01-23 20:37:53 +0900

    views updated for queryset

--- secondproject/blog/views.py
+++ secondproject/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import blog
 
 # Create your views here.
+def home(request):
+    blogs = blog.objects
+    return render(request, 'home.html', {'blogs': blogs})
