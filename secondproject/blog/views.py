e2d7fc4c49e49ed38a03c57744e54c39657e5a47
caesiumy <mn0658@naver.com>
2019-01-30 20:56:30 +0900

    style: Add , in views.detail & Modify url path in detail

--- secondproject/blog/views.py
+++ secondproject/blog/views.py
@@ -11,4 +11,4 @@ def home(request):
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'detail.html' {'blog': blog_detail})
+    return render(request, 'detail.html', {'blog': blog_detail})
