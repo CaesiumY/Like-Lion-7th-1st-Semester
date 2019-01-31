b64a24330bc54c7f0c1d39425164b208f8bffb2f
caesiumy <mn0658@naver.com>
2019-01-31 15:59:37 +0900

    feat: Add summary[:100]

--- secondproject/blog/models.py
+++ secondproject/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Blog(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=200)
@@ -9,3 +11,6 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def summary(self):
+        return self.body[:100]
