55e7c2b084d512500f793770ef8f712bf4b5c239
caesiumy <mn0658@naver.com>
2019-01-31 16:38:41 +0900

    refactor: test

--- secondproject/blog/models.py
+++ secondproject/blog/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Blog(models.Model):
     objects = models.Manager()
+    # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
+    # created_date = models.DateTimeField(
+    #     default=timezone.now)
     body = models.TextField()
 
     def __str__(self):
@@ -14,3 +18,7 @@ class Blog(models.Model):
 
     def summary(self):
         return self.body[:100]
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
