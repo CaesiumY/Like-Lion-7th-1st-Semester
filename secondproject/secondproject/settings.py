cb2e28aee29072a7f9a323c14372cc0fd9ed0dde
caesiumy <mn0658@naver.com>
2019-01-31 21:42:39 +0900

    refactor: Collect static

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'portfolio', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
