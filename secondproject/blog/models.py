d4db0dd40310a6e101ef6d8341c8f96bfbe760da
caesiumy <mn0658@naver.com>
2019-01-31 17:08:57 +0900

    feat: Change timezone to kor

--- secondproject/blog/models.py
+++ secondproject/blog/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Blog(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date_published')
+    pub_date = models.DateTimeField()
     body = models.TextField()
 
     def __str__(self):
