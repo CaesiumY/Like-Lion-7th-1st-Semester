472f460ac2666cf399178bf0df507eb5b8764824
caesiumy <mn0658@naver.com>
2019-02-01 23:38:16 +0900

    feat: Add basis of media model

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -120,9 +120,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio', 'static'),
     os.path.join(BASE_DIR, 'secondproject', 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
