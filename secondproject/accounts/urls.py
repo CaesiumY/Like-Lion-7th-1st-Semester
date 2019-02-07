dd91368971ba17dcaf65ede693b90610ddc74539
caesiumy <mn0658@naver.com>
2019-02-07 23:45:15 +0900

    feat: Add Login system

--- secondproject/accounts/urls.py
+++ secondproject/accounts/urls.py
@@ -4,4 +4,5 @@ import accounts.views
 urlpatterns = [
     path('signup/', accounts.views.signup, name="signup"),
     path('login/', accounts.views.login, name="login"),
+    path('logout/', accounts.views.logout, name="logout")
 ]
