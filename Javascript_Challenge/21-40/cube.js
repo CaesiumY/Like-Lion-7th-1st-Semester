53abf5e09a30f006d9570cd1bcb1501790bb4ba8
caesiumy <mn0658@naver.com>
2019-02-16 15:12:30 +0900

    feat: Add Javascript challenge 21 to 30

---
+++ Javascript_Challenge/21-40/cube.js
@@ -0,0 +1,11 @@
+// Write a function called "cube".
+
+// Given a number, "cube" returns the cube of that number.
+
+// var output = cube(3);
+// console.log(output); // --> 27
+
+function cube(num) {
+    // your code here
+    return num * num * num // Math.pow(num, num)
+}
