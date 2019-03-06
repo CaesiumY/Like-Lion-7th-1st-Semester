5c0566485afb3e9ae043a6e1c0d3ecdb3279a4fe
caesiumy <mn0658@naver.com>
2019-03-06 15:38:58 +0900

    feat: Add automations to make & count js files

---
+++ Javascript_Challenge/count.py
@@ -0,0 +1,27 @@
+import os
+
+list = sorted(list(filter(os.path.isdir, os.listdir(os.getcwd()))))
+count = 0
+sum = 0
+
+for pwd in list:
+    for path, dirs, files in os.walk("./" + str(pwd)):
+        count = 0
+        list = []
+
+        print("===== Folder =====")
+        print("|       ./" + str(pwd) + "       |")
+        print("====== List ======")
+
+        for file in files:
+            if os.path.splitext(file)[1].lower() == '.js':
+                list.append(file)
+                count += 1
+
+        sum += count
+        print(sorted(list))
+
+        print("Count : ", count)
+        print()
+
+print("Total count : " + str(sum))
