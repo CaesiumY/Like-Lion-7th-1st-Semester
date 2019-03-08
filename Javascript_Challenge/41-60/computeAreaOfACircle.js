ceda77103726c131c1d6d384b0a28202f74a1e11
caesiumy <mn0658@naver.com>
2019-03-08 21:42:33 +0900

    feat: Add javascript challenge 41 to 50

---
+++ Javascript_Challenge/41-60/computeAreaOfACircle.js
@@ -0,0 +1,11 @@
+// Write a function called "computeAreaOfACircle".
+
+// Given the radius of a circle, "computeAreaOfACircle" returns its area.
+
+// var output = computeAreaOfACircle(4);
+// console.log(output); // --> 50.26548245743669
+
+function computeAreaOfACircle(radius) {
+    // your code here
+    return Math.PI * radius * radius
+}
---
+++ Javascript_Challenge/41-60/computeSquareRoot.js
@@ -0,0 +1,11 @@
+// Write a function called "computeSquareRoot".
+
+// Given a number, "computeSquareRoot" returns its square root.
+
+// var output = computeSquareRoot(9);
+// console.log(output); // --> 3
+
+function computeSquareRoot(num) {
+    // your code here
+    return Math.sqrt(num)
+}
