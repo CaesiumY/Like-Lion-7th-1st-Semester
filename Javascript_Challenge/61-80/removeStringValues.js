62c784a7e92b675e9c7e80ca6664c0e24f5c2ef7
caesiumy <mn0658@naver.com>
2019-03-13 23:33:08 +0900

    feat: Add javascript challenge 71 to 80

---
+++ Javascript_Challenge/61-80/removeStringValues.js
@@ -0,0 +1,19 @@
+// Write a function called "removeStringValues".
+
+// Given an object, "removeStringValues" removes any properties on the given object whose values are strings.
+
+// var obj = {
+//   name: 'Sam',
+//   age: 20
+// }
+// removeStringValues(obj);
+// console.log(obj); // { age: 20 }
+
+function removeStringValues(obj) {
+    // your code here
+    for (var prop in obj) {
+        if (typeof obj[prop] === 'string') {
+            delete obj[prop]
+        }
+    }
+}
---
+++ Javascript_Challenge/61-80/removeStringValuesLongerThan.js
@@ -0,0 +1,28 @@
+// Write a function called "removeStringValuesLongerThan".
+
+// Given an number and an object, "removeStringValuesLongerThan" removes any properties on the given object whose values are strings longer than the given number.
+
+// var obj = {
+//   name: 'Montana',
+//   age: 20,
+//   location: 'Texas'
+// };
+// removeStringValuesLongerThan(6, obj);
+// console.log(obj); // { age: 20, location: 'Texas' }
+
+function removeStringValuesLongerThan(num, obj) {
+    // your code here
+    for (var prop in obj) {
+        if (obj[prop].length > num) {
+            delete obj[prop]
+        }
+    }
+}
+
+var obj = {
+    name: 'Montana',
+    age: 20,
+    location: 'Texas'
+};
+removeStringValuesLongerThan(6, obj);
+console.log(obj); // { age: 20, location: 'Texas' }
